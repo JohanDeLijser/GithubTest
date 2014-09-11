4014e49705f9cdd0b756a2afbb05fd9f6601aee1
peterbas010 <peterbas0102@live.nl>
2014-09-11 15:07:40 +0200

    "piece of" deleted

--- Githubtest.php
+++ Githubtest.php
@@ -6,6 +6,6 @@
 </head>
 <body>
 	<h1>Githubtest</h1>
-	<p>Gummies lemon drops piece of brownie marzipan dragée marzipan topping. Cheesecake jujubes candy. Bear claw bear claw candy canes pudding lemon drops tart. Chocolate bar marshmallow pie cotton candy oat cake liquorice. Chocolate bar icing lollipop cake powder oat cake cookie. Cake bear claw powder jujubes biscuit toffee bear claw sesame snaps dessert.</p>
+	<p>Gummies lemon drops brownie marzipan dragée marzipan topping. Cheesecake jujubes candy. Bear claw bear claw candy canes pudding lemon drops tart. Chocolate bar marshmallow pie cotton candy oat cake liquorice. Chocolate bar icing lollipop cake powder oat cake cookie. Cake bear claw powder jujubes biscuit toffee bear claw sesame snaps dessert.</p>
 </body>
 </html>
