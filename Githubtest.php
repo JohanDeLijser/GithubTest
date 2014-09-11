31515c699b198296f0bb683c6feca34733d342e5
peterbas010 <peterbas0102@live.nl>
2014-09-11 15:02:52 +0200

    Mijn eerste commit

---
+++ Githubtest.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Githubtest</title>
+</head>
+<body>
+	<h1>Githubtest</h1>
+	<p>Gummies lemon drops brownie marzipan dragée marzipan topping. Cheesecake jujubes candy. Bear claw bear claw candy canes pudding lemon drops tart. Chocolate bar marshmallow pie cotton candy oat cake liquorice. Chocolate bar icing lollipop cake powder oat cake cookie. Cake bear claw powder jujubes biscuit toffee bear claw sesame snaps dessert.</p>
+</body>
+</html>
