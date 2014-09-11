e349d55f6f6dd925343e9e5f8877ee3904e5153d
JohanDeLijser <jdelijser@live.nl>
2014-09-11 15:31:29 +0200

    verandering

--- Githubtest.php
+++ Githubtest.php
@@ -5,7 +5,7 @@
 	<title>Githubtest</title>
 </head>
 <body>
-	<h1>GithubNoobTest</h1>
+	<h1>GithubTest</h1>
 	<p>Gummies lemon drops brownie marzipan dragée marzipan topping. Cheesecake jujubes candy. Bear claw bear claw candy canes pudding lemon drops tart. Chocolate bar marshmallow pie cotton candy oat cake liquorice. Chocolate bar icing lollipop cake powder oat cake cookie. Cake bear claw powder jujubes biscuit toffee bear claw sesame snaps dessert.</p>
 </body>
 </html>
